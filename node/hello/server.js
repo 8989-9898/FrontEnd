dc08a20dc415ad84bdd7c7065cae4b3b389f5323
8989-9898 <1475304280@qq.com>
2019-10-04 13:12:08 +0800

    提交修改

--- node/hello/server.js
+++ node/hello/server.js
@@ -3,7 +3,9 @@ http.createServer(function (request, response) {
     // 发送头部
     // HTTP 状态值 200 ： OK
     // 内容类型 ：text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
 
     // 发送相应数据
     response.end('Hello World \n');
